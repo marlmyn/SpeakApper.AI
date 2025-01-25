9e89be8cb7dd29eb854491caf0c7b3a3ab276b0d
Akmaral <e.akmaral024@mail.ru>
2025-01-25 21:05:39 +0500

    Initial Commit

---
+++ SpeakApper.AI/SpeakApper_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApper_AIApp.swift
+//  SpeakApper.AI
+//
+//  Created by Akmaral Ergesh on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeakApper_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeakApper.AITests/SpeakApper_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApper_AITests.swift
+//  SpeakApper.AITests
+//
+//  Created by Akmaral Ergesh on 25.01.2025.
+//
+
+import Testing
+@testable import SpeakApper_AI
+
+struct SpeakApper_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
